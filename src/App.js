6f778160723ef8e0d980ef8476b7decb99303735
just2102 <thesixteensaltines@gmail.com>
2023-01-21 12:31:10 +0900

    Style textarea

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Welcome from "./Components/Welcome/Welcome";
-import Chatbox from "./Components/Chatbox/Chatbox";
+// import Chatbox from "./Components/Chatbox/Chatbox";
+//import SendMessage from "./Components/Message/SendMessage";
 import "./App.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
 import Preloader from "./Components/Common/Preloader";
 import Footer from "./Components/Footer/Footer";
-import SendMessage from "./Components/Message/SendMessage";
+
+import { lazy, Suspense } from "react";
+
+const Chatbox = lazy(()=> import('./Components/Chatbox/Chatbox'))
+const SendMessage = lazy(()=> import('./Components/Message/SendMessage'))
+
 function App() {
   const [user] = useAuthState(auth);
   return (
     <div className="app_container">
+    <Suspense fallback={<Preloader/>}>
       <Navbar user={user} />
       {!user && <Welcome />}
-      {/* {user === null && <Preloader />} */}
+
       {user && <Chatbox user={user} />}
       {user && <SendMessage user={user} />}
+    </Suspense>
       <Footer></Footer>
     </div>
   );
--- src/Components/Common/Preloader.jsx
+++ src/Components/Common/Preloader.jsx
@@ -2,7 +2,11 @@ import preloader from "../../img/Spinner-0.7s-237px.svg"
 
 const Preloader = () => {
     return ( 
-        <img src={preloader} alt="" />
+        <div className="preloader">
+            Loading...
+            <img src={preloader} alt="" />
+        </div>
+        
      );
 }
  
